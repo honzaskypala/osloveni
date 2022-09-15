5ca055e30fdb4f029dc1bb526f5a4547888aef67
Honza Skýpala <honza@honza.info>
2022-09-15 18:02:04 +0200

    Removed unnecessary variable

--- generate-code/Tests/OsloveniTest.php
+++ generate-code/Tests/OsloveniTest.php
@@ -6,7 +6,6 @@ final class OsloveniTest extends TestCase
 {
     public function testOsloveni(): void {
         $handle = fopen("../words.txt", "r");
-        $values = [];
         for ($i = 0; $row = fgetcsv($handle ); ++$i) {
             if (strlen($row[0]) > 2 && ord($row[0][0]) == 239 && ord($row[0][1]) == 187 && ord($row[0][2]) == 191)
                 $row[0] = substr($row[0], 3);  // strip UTF-8 file header
