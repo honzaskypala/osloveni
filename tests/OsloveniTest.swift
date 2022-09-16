36f47543cf85af217258ac12b67109e5d8747c96
Honza Skýpala <honza@honza.info>
2022-09-16 21:49:56 +0200

    Swift unit test added

---
+++ tests/OsloveniTest.swift
@@ -0,0 +1,9 @@
+func testOsloveni(words: String) throws {
+    let lines = words.split(whereSeparator: \.isNewline)
+    for i in 0..<lines.count {
+        let wordPair = String(lines[i]).components(separatedBy: ";")
+        XCTAssertEqual(osloveni(wordPair[0].uppercased()), wordPair[1].uppercased())
+        XCTAssertEqual(osloveni(wordPair[0].lowercased()), wordPair[1].lowercased())
+        XCTAssertEqual(osloveni(wordPair[0].capitalized), wordPair[1].capitalized)
+    }
+}
