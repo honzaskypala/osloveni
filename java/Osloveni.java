71d08aeaa3980a8186c11fe7164491f34566e0d0
Karlos174 <116342887+Karlos174@users.noreply.github.com>
2023-10-14 17:43:40 +0200

    Fixed empty string comparision
    Note that this is not generated code but it is manually fixed.

--- java/Osloveni.java
+++ java/Osloveni.java
@@ -495,11 +495,11 @@ public static String osloveni(String jmeno) {
         replacepair = new String[]{"", "i"};
     }
 
-    if (replacepair[0] == "" && replacepair[1] == "") {
+    if (replacepair[0].isEmpty() && replacepair[1].isEmpty()) {
         return jmeno;
-    } else if (replacepair[1] == "") {
+    } else if (replacepair[1].isEmpty()) {
         return jmeno.substring(0, jmeno.length() - replacepair[0].length());
-    } else if (replacepair[0] == "") {
+    } else if (replacepair[0].isEmpty()) {
         return jmeno + ((String.valueOf(jmeno.charAt(jmeno.length() - 1)).toLowerCase().equals(jmeno.charAt(jmeno.length() - 1))) ? replacepair[1] : replacepair[1].toUpperCase());
     } else {
         String replaceending;
